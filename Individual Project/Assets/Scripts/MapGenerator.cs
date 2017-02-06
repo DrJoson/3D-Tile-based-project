5b142f96cef7506016f5c9a9f6c44eccc0c9f17f
Joseph Thomas <joelukethomas@live.co.uk>
2017-02-06 23:22:37 +0000

    Map Generation
    The game controller now asks a map generation script to generate the
    game map during runtime

---
+++ Individual Project/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour {
+	
+	public TileType[] tileTypes;
+	
+	int[,] tiles;
+	
+	int mapSizeX = 10;
+	int mapSizeZ = 10;
+	
+	public void GenerateMap(){
+		GenerateMapData();
+		GenerateTiles();
+	}
+	
+	void GenerateMapData(){
+		tiles = new int[mapSizeX,mapSizeZ];
+		
+		// defaulting all map to be made of grass tiles		
+		for(int x=0; x < mapSizeX; x++){
+			for(int z=0; z < mapSizeZ; z++){
+				tiles[x,z] = 0;
+			}
+		}
+		// setting some forest tiles
+		tiles[0, 2] = 1;
+		tiles[0, 3] = 1;
+		tiles[1, 2] = 1;
+		tiles[1, 3] = 1;
+		tiles[2, 2] = 1;
+		tiles[2, 3] = 1;
+		
+		// setting some stone tiles
+		tiles[5, 2] = 2;
+		tiles[5, 3] = 2;
+		tiles[5, 4] = 2;
+		tiles[6, 3] = 2;
+		tiles[6, 4] = 2;
+		
+	}
+	
+	void GenerateTiles (){
+		for(int x=0; x < mapSizeX; x++){
+			for(int z=0; z < mapSizeZ; z++){
+				TileType tt = tileTypes[tiles[x,z]];
+				Instantiate(tt.tilePrefab, new Vector3(x, 0, z), Quaternion.identity);
+			}
+		}
+	}
+}
